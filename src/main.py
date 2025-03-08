c2ae5767b25f411ca68861308af39e0f232d0fa5
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2025-03-08 20:51:56 -0300

    docs(OpenAPI): :memo: Se agrego config de openapi en el main del proyecto

--- src/main.py
+++ src/main.py
@@ -3,7 +3,26 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.shared.infrastructure.http.routes import base_router
 
-app = FastAPI()
+app = FastAPI(
+    title="SyncLife API",
+    description="This is the API that contains all of SyncLife's business logic.",
+    version="0.0.1",
+    contact={
+        "name": "SyncLife Support",
+        "email": "support@synclife.com",
+    },
+    docs_url="/api/docs/swagger",
+    redoc_url="/api/docs/redoc",
+    openapi_url="/api/docs/openapi.json",
+    # swagger_ui_init_oauth={
+    #     "clientId": "tu_client_id",
+    #     "clientSecret": "tu_client_secret",
+    #     "realm": "tu_realm",
+    #     "appName": "nombre_de_tu_app",
+    #     "scopeSeparator": " ",
+    #     "additionalQueryStringParams": {"test": "hello"}
+    # }
+)
 
 app.add_middleware(
     CORSMiddleware,
